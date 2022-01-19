99e45f9b613a82dca1836294f0c96fb81ed5a68f
benjamin amakye <bamakye20@gmail.com>
2022-01-19 18:35:52 -0500

    fourth commit

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
-const async = require("async");
+const utils= require("utils");
 
 // TODO: Create an array of questions for user input
 const questions = [
